7d7faa1344136856bb6cc31aea94a19cba75f7f4
Christian G <chrisgobin94@gmail.com>
2021-12-19 01:42:31 -0500

    Update higherOrder.js

--- vanillaJavaScript/higherOrder.js
+++ vanillaJavaScript/higherOrder.js
@@ -12,3 +12,17 @@ isInBetween = (val1, val2) => {
 
 adultCheck = isInBetween(0, 21);
 console.log(adultCheck(19));
+
+
+multiplyByParam = (x) => {
+	return(
+		(num) => {
+			return num * x;
+		}
+	)
+}
+
+timesThree = multiplyByParam(3);
+timesNine = multiplyByParam(9);
+console.log(timesThree(3));
+console.log(timesNine(3));
