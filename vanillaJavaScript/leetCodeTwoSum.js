64fc72bc670bd259e6036d75c831530740df2b75
Christian G <chrisgobin94@gmail.com>
2021-12-19 11:25:07 -0500

    Update leetCodeTwoSum.js

--- vanillaJavaScript/leetCodeTwoSum.js
+++ vanillaJavaScript/leetCodeTwoSum.js
@@ -15,3 +15,14 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+/* 
+a. Sort List in ascending order
+b. Start at the first(f) and last positions in a list(l).
+c. Add values until target is reached or consensus that the combination does not exist in the list, 
+    if(f + l < target) -> move f to next position to the right of the first element.
+    if(f+l > target) -> move l to the next position to the left of the last element.
+d. Keep running until target is reached.
+e. Log positions of key pair that reaches target value.
+
+*/
