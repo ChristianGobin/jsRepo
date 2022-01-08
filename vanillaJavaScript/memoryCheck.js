e7d5618f58b4ab716eb55741b20937fd8a285934
Christian G <chrisgobin94@gmail.com>
2022-01-08 15:02:15 -0500

    Update memoryCheck.js

--- vanillaJavaScript/memoryCheck.js
+++ vanillaJavaScript/memoryCheck.js
@@ -1,12 +1,59 @@
-// Construct a class, object, higher order function, variable
+// Object
+// Class
+// Higher Order Functions
 
-higherOrder = (x) => {
-	return (z) => {
-		if(x >= 10){
-			return true;
-		} else {
-			return false;
-		}
+let Student = {
+	'NAME' : "Christian",
+	'AGE' : 27
+}
+
+class StudentClass {
+	setName = (name) => {
+		this.Name = name;
 	}
 }
 
+console.log(Student['NAME']); //Student Object.
+
+const Christian = new StudentClass(); //StudentClass.
+
+Christian.setName("Christian");
+console.log(Christian.Name);
+
+
+// Function as arguments
+
+letThis = (func1 , x) => {
+	func1(x);
+}
+
+letThis(console.log, 3);
+
+// Higher Order Functions
+
+myFunc = (z) => {
+	return (x) => {
+		console.log(x + z);
+	}
+}
+
+let AddTo4 = myFunc(4); // Function sets z to 4 in outer function, returns inner function without x
+AddTo4(3);
+
+add = (x, y) => {
+	console.log(x+y);
+}
+
+sub = (x, y) => {
+	console.log(x-y);
+}
+
+mult = (x, y) => {
+	console.log(x*y);
+}
+
+div = (x, y) => {
+	console.log(x/y);
+}
+console.log(animal);
+var animal = "Dog";
