52fcd271a9e6df5aab9a7fe06608baea4945e082
Christian G <chrisgobin94@gmail.com>
2022-02-24 12:54:59 -0500

    Create CodeWarsOutlier.js

---
+++ vanillaJavaScript/CodeWarsOutlier.js
@@ -0,0 +1,16 @@
+//Find the outlier in an array of numbers. Whether even or odd.
+
+let arrayInts = [22, 45, 12, 44];
+let oddCount = 0;
+let evenCount = 0;
+for(num in arrayInts){
+    if(arrayInts[num] % 2 == 0){
+        evenCount++;
+    } else {
+        oddCount++;
+    }
+}
+
+function findOutlier(numArray){
+  pass
+}
