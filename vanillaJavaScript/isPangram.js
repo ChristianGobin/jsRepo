f4d835b330585c1292932e23111b8a7c4f8c2cfa
Christian G <chrisgobin94@gmail.com>
2022-02-24 12:56:32 -0500

    Update isPangram.js

--- vanillaJavaScript/isPangram.js
+++ vanillaJavaScript/isPangram.js
@@ -4,8 +4,7 @@ let someString ="this is some string";
 isPanam = (userString) => {
   let res = true;
   const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  //need to use a foreach to search the alphabet array
-  //for(let i : userString){
-  //  console.log(i);
-  //}
+  for(i in alphabet){
+    //Do Something.
+  }
 };
