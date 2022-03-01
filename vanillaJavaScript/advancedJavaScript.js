5a3de50353590a3dd17d132597842fbdd36f3fca
Christian G <chrisgobin94@gmail.com>
2022-02-28 23:58:47 -0500

    Update advancedJavaScript.js

--- vanillaJavaScript/advancedJavaScript.js
+++ vanillaJavaScript/advancedJavaScript.js
@@ -2,3 +2,15 @@
 
 class Person {}
 class Student extends Person {}
+
+const someObject = {
+	name : '',
+	age : 0,
+	sayName : function(){
+		console.log(this.name);
+	}
+}
+
+let newObject = someObject;
+newObject.name = "Christian";
+newObject.sayName();
